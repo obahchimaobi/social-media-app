7ba9b434383a1888d8480f8632015395d1339d2b
obahchimaobi <anthonyobah37@gmail.com>
2024-12-31 14:41:31 +0100

    Created Livewire Components and Designed the Authentication Forms

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Livewire\Auth\LoginForm;
+use App\Livewire\Auth\RegisterForm;
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Auth\ForgotPasswordForm;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware('auth');
+
+Route::get('/login', LoginForm::class)->name('login');
+Route::get('/register', RegisterForm::class)->name('register');
+Route::get('/forgot-password', ForgotPasswordForm::class)->name('forgot.password');
--- tailwind.config.js
+++ tailwind.config.js
@@ -19,5 +19,6 @@ export default {
     },
     plugins: [
         require('preline/plugin'),
+        require('@tailwindcss/forms'),
     ],
 };
